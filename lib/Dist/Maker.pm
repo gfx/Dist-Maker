ea6a5e4f80ba574e0ddb48a5c990adb984fdc5a1
Fuji Goro <fuji.goro@dena.jp>
2011-08-25 16:31:26 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 3d89524..8a687ff 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Dist::Maker
    
    +0.04 2011-08-25 16:28:38
    +    - Fix typo in the pod template
    +
     0.03 2011-07-27 10:38:48
         - Re-packaging for missing M::I::Repository

--- lib/Dist/Maker.pm
+++ lib/Dist/Maker.pm
@@ -3,7 +3,7 @@ use 5.10.0;
 use strict;
 use warnings;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use Dist::Maker::Config;
 use Dist::Maker::Util qw(parse_options);
@@ -51,7 +51,7 @@ Dist::Maker - Yet another distribution maker
 
 =head1 VERSION
 
-This document describes Dist::Maker version 0.03.
+This document describes Dist::Maker version 0.04.
 
 =head1 SYNOPSIS
 
